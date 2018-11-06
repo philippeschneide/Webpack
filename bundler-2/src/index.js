6dbcf8bb5339e8963961c893b015623fd7d686bc
javed khalil <admin@techmynd.com>
2018-11-06 10:22:12 +0500

    update

---
+++ bundler-2/src/index.js
@@ -0,0 +1,4 @@
+import greet from "./greeting";
+require("./another-file");
+console.log("hello world");
+console.log(greet("Bob"));
---
+++ bundler-2/webpack.config.js
@@ -0,0 +1,20 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+const BabiliPlugin = require("babili-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "main.min.js"
+  },
+  plugins: debug ? [] : [
+    new BabiliPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
