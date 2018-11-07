d93eeb610642adadb65aee0ffdc4eb0f61f4a996
javed khalil <admin@techmynd.com>
2018-11-07 14:40:00 +0500

    update

---
+++ js-dev-env/build_scripts/startMessage-launch-live-public.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var chalk = require('chalk');
+console.log(chalk.green('######################################'));
+console.log(chalk.green('Firing up live server ...'));
+console.log(chalk.green('Go to https://mjklive.localtunnel.me'));
+console.log(chalk.green('######################################'));
---
+++ js-dev-env/build_scripts/startMessage-launch-local-express.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var chalk = require('chalk');
+console.log(chalk.green('############################'));
+console.log(chalk.green('Starting localhost ...'));
+console.log(chalk.green('Go to http://localhost:3000'));
+console.log(chalk.green('############################'));
