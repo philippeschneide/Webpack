c8b5a5f02ae48b8442b5d517b2d85fca33932959
javed khalil <admin@techmynd.com>
2018-11-11 06:50:09 +0500

    update

---
+++ 0-webpack-4/via-config/webpack.config.js
@@ -0,0 +1,31 @@
+import path from 'path';
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
---
+++ 0-webpack-4/via-scripts/src/index.js
@@ -0,0 +1,9 @@
+console.log('Hello World from wfwwindexjs');
+console.log('Hello World eewww  ererre');
+
+let Func = (a, b = 10) => {
+ return a + b; 
+}
+let displayresult = Func(20); // 20 + 10 = 30
+
+console.log(displayresult);
